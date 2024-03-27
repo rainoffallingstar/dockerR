f082a9b687df106559688107f5d51caead2f491f
Yanhua Zheng <36413792+rainoffallingstar@users.noreply.github.com>
2024-03-27 10:06:13 +0800

    update example

--- dev/example/container-autoupdate.R
+++ dev/example/container-autoupdate.R
@@ -5,5 +5,5 @@ dockerR::container_update(containerid = "lobe-chat",
                           ports = "-p 3210:3210",
                           environmentvarible = "-e OPENAI_API_KEY=sk-97sVg41KF1zHyqrlB0C892BfC4824d8cA64cFaE15b312fE7 -e OPENAI_PROXY_URL=https://api.aigcapi.io/v1 -e ACCESS_CODE=fallingstar -e OLLAMA_PROXY_URL=http://host.docker.internal:11434/v1",
                           ifD = TRUE,
-                          restart = FALSE,
+                          restart = TRUE,
                           run_at_once = TRUE)
